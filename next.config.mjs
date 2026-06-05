0d22828bd56984e4fef81ea53ec9d562b2890659
User <user@myapp.com>
2026-06-05 13:58:00 +0900

    Remove next-pwa to fix Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import withPWA from 'next-pwa'
-
-const pwa = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/middleware-manifest\.json$/],
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-export default pwa(nextConfig)
+export default nextConfig
